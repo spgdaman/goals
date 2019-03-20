62d055cf26dbbf249b83ed824c91fee088a7e7ad
spgdaman <gatheru.simon432@gmail.com>
2019-03-20 16:12:17 +0300

    Commit before push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Goal } from './goal'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  title = 'Goals';
+  // goals: string[];
+  //
+  // constructor(){
+  //
+  //   this.goals = ["Watch Finding Nemo","Buy Cookies","Get new phone case"];
+  // }
+
 }
